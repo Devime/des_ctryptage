7b31f0bbe8c611b494b420fc21b1697c55c0a8f4
Ashley <al.ashley@laposte.net>
2021-03-01 14:44:36 +0400

    code chiffrement de césar

---
+++ app/src/main/java/com/example/des_ctryptage/cesar.kt
@@ -0,0 +1,82 @@
+package com.example.des_ctryptage.com.example.des_ctryptage
+
+class cesar {
+    var alphabet=charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    //fonction pour crypter
+    fun crypter(n: Int, message_entre: String): String {
+        val message=message_entre.toCharArray()
+        val message_sortie=CharArray(message.size)
+        var position_de_depart: Int
+        var nouvelle_position: Int
+        for (i in message.indices) {
+            position_de_depart= depart(message[i], alphabet)
+            nouvelle_position= newp(position_de_depart, n)
+            if (nouvelle_position==-1)
+                message_sortie[i]=' '
+            else
+                message_sortie[i]= alphabet[nouvelle_position]
+        }
+        return String(message_sortie)//conversion en string d'un tableau de char
+    }
+//fonction pour décrypter
+    fun decrypter(n: Int, message_entre: String): String {
+        val message=message_entre.toCharArray()
+        val message_sortie=CharArray(message.size)
+        var position_de_depart: Int
+        var nouvelle_position: Int
+        for (i in message.indices) {
+            position_de_depart= depart(message[i], alphabet)
+            nouvelle_position= newp(position_de_depart, -n)
+            if (nouvelle_position==-1)
+                message_sortie[i]=' '
+            else message_sortie[i]= alphabet[nouvelle_position]
+        }
+        return String(message_sortie)
+    }
+    //position de départ
+    private fun depart(c: Char, tab: CharArray): Int {
+        for (i in tab.indices) {
+            if (tab[i] == c)
+                return i
+        }
+        return -1
+    }
+//nouvelle position
+    private fun newp(pos: Int, n: Int): Int {
+        var nouvelle_position=pos
+        if (pos<=-1) {
+            nouvelle_position=-1
+        } else {
+            var i=0
+            while (i<absolue(n)) {
+                if (n<0) {
+                    if (nouvelle_position-1==-1) nouvelle_position=25
+                    else
+                        nouvelle_position--
+                } else {
+                    if (nouvelle_position+1>=25)
+                        nouvelle_position=0
+                    else
+                        nouvelle_position++
+                }
+                i++
+            }
+        }
+        return nouvelle_position
+    }
+    // Valeur absolue
+    fun absolue(a: Int): Int {
+        return if (a>=0)
+            a
+        else
+            -a
+    }
+    //fonction de teste
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val t = "je suis nulle"
+        val f="mh vxlv qxooh"
+        println(crypter(3, t))
+        println(decrypter(3, f))
+    }
+}
